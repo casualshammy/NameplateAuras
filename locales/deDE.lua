ef0c8eadde2d013256cbe3cabb791f452b7dac89
unknown <axio@axio.name>
2016-11-18 09:29:43 +0000

    Added deDE localization, thank you pas06! Added zhCN localization, thank you yaoenqi! Added option to show/hide aura on nameplates of allies/enemies (Options -> Spells) (Ticket #...erm...ticket tracker is not working)

--- locales/deDE.lua
+++ locales/deDE.lua
@@ -1,3 +1,97 @@
 local L = LibStub("AceLocale-3.0"):NewLocale("NameplateAuras", "deDE");
 if not L then return end
-L["Magic"] = "Magie";
+L["< 1min"] = "< 1 Min."
+L["< 5sec"] = "< 5 Sek."
+L["> 1min"] = "> 1 Min."
+L["Add new spell: "] = "Neuen Zauber hinzufügen:"
+L["Add spell"] = "Zauber hinzufügen"
+--Translation missing 
+L["All auras"] = "All auras"
+L["Always display icons at full opacity (ReloadUI is required)"] = "Symbole immer mit voller Deckkraft anzeigen (UI-Neuladen erforderlich)"
+L["Always show auras cast by myself"] = "Auren, die ich gewirkt habe, immer anzeigen"
+L["Anchor point"] = "Ankerpunkt"
+--Translation missing 
+L["Anchor to icon"] = "Anchor to icon"
+L["Aura type"] = "Auratyp"
+L["Border thickness"] = "Randdicke"
+--Translation missing 
+L["By aura type (de/buff) + expire time"] = "By aura type (de/buff) + expire time"
+L["By expire time, ascending"] = "Nach Ablaufzeit, zunehmend"
+L["By expire time, descending"] = "Nach Ablaufzeit, abnehmend"
+L["By icon size, ascending"] = "Nach Symbolgröße, zunehmend"
+L["By icon size, descending"] = "Nach Symbolgröße, abnehmend"
+--Translation missing 
+L["Check spell ID"] = "Check spell ID"
+L["Circular"] = "Kreisförmig"
+L["Circular with OmniCC support"] = "Kreisförmig mit OmniCC-Unterstützung"
+L["Circular with timer"] = "Kreisförmig mit Timer"
+L["Click to select spell"] = "Klicken, um einen Zauber auszuwählen"
+L["Curse"] = "Fluch"
+L["Default icon size"] = "Standard-Symbolgröße"
+L["Delete all spells"] = "Alle Zauber entfernen"
+L["Delete spell"] = "Zauber löschen"
+--Translation missing 
+L["Disabled"] = "Disabled"
+L["Disease"] = "Krankheit"
+L["Display auras on nameplates of friendly units"] = "Auren auf der Namensplakette verbündeter Einheiten anzeigen"
+L["Display auras on player's nameplate"] = "Auren auf der Namensplakette des Spieler anzeigen"
+L["Display tenths of seconds"] = "Zehntelsekunden anzeigen"
+L["Do you really want to delete ALL spells?"] = "Willst du wirklich ALLE Zauber entfernen?"
+--Translation missing 
+L["Font"] = "Font"
+L["Font scale"] = "Schriftskalierung"
+L["Font size"] = "Schriftgröße"
+--Translation missing 
+L["Frame anchor:"] = "Frame anchor:"
+L["General"] = "Allgemein"
+L["Hide Blizzard's aura frames (Reload UI is required)"] = "Blizzards Auraelemente ausblenden (UI-Neuladen erforderlich)"
+L["Icon anchor:"] = "Symbolanker:"
+L["Icon borders"] = "Symbolrahmen"
+L["Icon size"] = "Symbolgröße"
+L["Icon X-coord offset"] = "Symbolverschiebung X-Richtung"
+L["Icon Y-coord offset"] = "Symbolverschiebung Y-Richtung"
+L["Magic"] = "Magie"
+L["Mode"] = "Modus"
+L["No"] = "Nein"
+--Translation missing 
+L["None"] = "None"
+--Translation missing 
+L["Only my auras"] = "Only my auras"
+--Translation missing 
+L["Open profiles dialog"] = "Open profiles dialog"
+L["Options are not available in combat!"] = "Optionen sind im Kampf nicht verfügbar!"
+L["Other"] = "Andere"
+L["Please reload UI to apply changes"] = "Bitte UI neuladen, um Änderungen zu übernehmen"
+L["Poison"] = "Gift"
+L["Profiles"] = "Profile"
+L["Reload UI"] = "UI neuladen"
+L[ [=[Scale font size
+according to
+icon size]=] ] = [=[Schriftgröße an
+Symbolgröße
+anpassen]=]
+L["Show border around buff icons"] = "Rahmen um Stärkungszaubersymbole zeigen"
+L["Show border around debuff icons"] = "Rahmen um Schwächungszaubersymbole zeigen"
+--Translation missing 
+L["Show this aura on nameplates of allies"] = "Show this aura on nameplates of allies"
+--Translation missing 
+L["Show this aura on nameplates of enemies"] = "Show this aura on nameplates of enemies"
+L["Sort mode:"] = "Anordnung:"
+L["Space between icons"] = "Platz zwischen Symbolen"
+L["Spell already exists (%s)"] = "Zauber existiert bereits (%s)"
+L["Spell seems to be nonexistent"] = "Zauber scheint nicht zu existieren"
+L["Spells"] = "Zauber"
+L["Stack text"] = "Stapeltext"
+L["Text"] = "Text"
+L["Text color"] = "Textfarbe"
+L["Texture with timer"] = "Textur mit Timer"
+L["Timer style:"] = "Timerstil:"
+L["Timer text"] = "Timertext"
+L["Unknown spell: %s"] = "Unbekannter Zauber: %s"
+L["Value must be a number"] = "Wert muss eine Zahl sein"
+L["X offset"] = "X-Verschiebung"
+L["Y offset"] = "Y-Verschiebung"
+L["Yes"] = "Ja"
+L[ [=[You should enter spell name instead of spell id.
+Use \"%s\" option if you want to track spell with specific id]=] ] = [=[Du solltest den Zaubernamen anstatt die Zauber-ID eingeben.
+Verwende die Option %s, wenn du Zauber mit einer bestimmten ID verfolgen möchtest.]=]
